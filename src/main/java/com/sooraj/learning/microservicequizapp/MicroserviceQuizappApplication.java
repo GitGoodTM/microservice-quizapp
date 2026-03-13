3b49f848a5cc6c68a4cf5f7dc461c76fd27db3b6
GitGoodTM <soorajkumar7798@gmail.com>
2026-03-13 12:04:36 +0530

    init commit

---
+++ src/main/java/com/sooraj/learning/microservicequizapp/MicroserviceQuizappApplication.java
@@ -0,0 +1,13 @@
+package com.sooraj.learning.microservicequizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceQuizappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceQuizappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sooraj/learning/microservicequizapp/MicroserviceQuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sooraj.learning.microservicequizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceQuizappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
